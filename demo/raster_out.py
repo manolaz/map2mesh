8aedd19c4aa69b38550a34cdd970479f4bad3c26
Tristan Nguyen <trungupdate@gmail.com>
2020-07-23 20:59:47 +0700

    raster2

--- demo/raster_out.py
+++ demo/raster_out.py
@@ -1,7 +1,12 @@
 import arcpy
 from arcpy import env
+import glob, os
 env.workspace = "CURRENT"
 arcpy.CheckOutExtension('3D')
-inputLASFile = "LAS.lasd"
+
+
+files = glob.glob(os.getcwd() + "/data/*.las")
+
+# inputLASFile = "LAS.lasd"
 outputRasterFile = "Raster"
-arcpy.LasDatasetToRaster_3d(inputLASFile, outputRasterFile, 'ELEVATION', 'TRIANGULATION LINEAR NONE')
+arcpy.LasDatasetToRaster_3d(files, outputRasterFile, 'ELEVATION', 'TRIANGULATION LINEAR NONE')
