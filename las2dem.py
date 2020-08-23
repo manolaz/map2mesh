5481fc76fbe84c48e73786a1353b22837845a760
Tristan Nguyen <trungupdate@gmail.com>
2020-08-24 02:27:46 +0700

    WhiteboxTools Sample

---
+++ las2dem.py
@@ -0,0 +1,58 @@
+import os
+from WBT.whitebox_tools import WhiteboxTools
+
+wbt = WhiteboxTools()
+
+# Set the working directory, i.e. the folder containing the data,
+# to the 'data' sub-directory.
+wbt.set_working_dir(os.path.dirname(os.path.abspath(__file__)) + "/data/")
+
+# When you're running mulitple tools, the outputs can be a tad
+# chatty. In this case, you may want to suppress the output by
+# setting the verbose mode to False.
+# wbt.set_verbose_mode(False)
+
+# Interpolate the LiDAR data using an inverse-distance weighting
+# (IDW) scheme.
+print("Interpolating DEM...")
+wbt.lidar_idw_interpolation(
+i="StElisAk.las",
+output="raw_dem.tif",
+parameter="elevation",
+returns="last",
+resolution=1.0,
+weight=1.0,
+radius=2.5
+)
+
+# The resulting DEM will contain NoData gaps. We need to fill
+# these in by interpolating across the gap.
+print("Filling missing data...")
+wbt.fill_missing_data(
+i="raw_dem.tif",
+output="dem_nodata_filled.tif",
+filter=11
+)
+
+# This DEM will contain grid cells that have no lower neighbours.
+# This condition is unsuited for flow-path modelling applications
+# because these operations assume that each interior cell in the
+# DEM has at least one downslope neighour. We'll use an operation
+# called depression breaching to 'fix' the elevations within the
+# DEM to enforce continuous flow.
+print("Performing flow enforcement...")
+wbt.breach_depressions(
+dem="dem_nodata_filled.tif",
+output="dem_hydro_enforced.tif"
+)
+
+# Lastly, perform the flow accumulation operation using the
+# D-infinity flow algorithm.
+print("Performing flow accumulation...")
+wbt.d_inf_flow_accumulation(
+dem="dem_hydro_enforced.tif",
+output="flow_accum.tif",
+log=True
+)
+
+print("Complete!")
